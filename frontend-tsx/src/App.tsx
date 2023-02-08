1aa4779f6b8bbfb1f86b36274075f9af028526a4
Spencer <dasminimiiii@gmail.com>
2023-02-08 21:13:36 +0100

    Added: TypeScript Support

---
+++ frontend-tsx/src/App.tsx
@@ -0,0 +1,13 @@
+import './App.css';
+import { Route, Routes } from 'react-router-dom';
+import Welcome from './pages/Welcome';
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Welcome/>}></Route>
+    </Routes>
+  );
+}
+
+export default App;
