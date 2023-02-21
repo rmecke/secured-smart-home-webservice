a6e35752eae7bff571aadae547316765af13e298
rmecke <104442477+rmecke@users.noreply.github.com>
2023-02-21 13:48:38 +0100

    Removed old frontend-folder, Added CORS

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,6 +1,7 @@
 import Fastify, { FastifyInstance, FastifyServerFactory } from 'fastify';
 import fetch from 'cross-fetch';
 var express = require("express");
+var cors = require('cors');
 var app = express();
 
 const ENDPOINTS = {
@@ -16,28 +17,28 @@ const PORT = process.env.PORT ? parseInt(process.env.PORT) : 54000;
 
 app.listen(PORT, () => {
     console.log(`REST API listens on port ${PORT}`)
-  })
+})
 
 app.get(ENDPOINTS.HEALTH, (req, res) => {
-    res.send(true);
+    res.status(200).send(true);
 });
 
 app.get(ENDPOINTS.ON, async (req, res) => {
     await fetch(`http://10.13.13.2:8087/set/mqtt.0.test.esp?value=true`)
-    res.send(true);
+    res.status(200).send(true);
 })
 
 app.get(ENDPOINTS.OFF, async (req, res) => {
     await fetch(`http://10.13.13.2:8087/set/mqtt.0.test.esp?value=false`)
-    res.send(true);
+    res.status(200).send(true);
 })
 
 app.get(ENDPOINTS.OPEN, async (req, res) => {
     await fetch(`http://10.13.13.2:8087/set/myq.0.devices.CG0811460676.commands.open?value=true`)
-    res.send(true);
+    res.status(200).send(true);
 })
 
 app.get(ENDPOINTS.CLOSE, async (req, res) => {
     await fetch(`http://10.13.13.2:8087/set/myq.0.devices.CG0811460676.commands.close?value=true`)
-    res.send(true);
+    res.status(200).send(true);
 })
