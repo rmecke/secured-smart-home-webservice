11d2141d5f65126b86ac298019418b10e8c765da
Spencer <dasminimiiii@gmail.com>
2023-03-09 15:41:21 +0100

    Added: Buzzer control

---
+++ home-automation-ui/src/components/Device/Controls/Buzzer/BuzzerControl.js
@@ -0,0 +1,30 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import Button from "../../../UI/Button/Button";
+import classes from "./BuzzerControl.module.scss";
+
+export default class BuzzerControl extends Component {
+  static propTypes = {
+    controlId: PropTypes.string,
+    name: PropTypes.string,
+    value: PropTypes.bool,
+    onUpdateValue: PropTypes.func,
+    options: PropTypes.object
+  };
+
+  onClickHandler = event => {
+    const updatedValue = true;
+    this.props.onUpdateValue(this.props.controlId, updatedValue);
+  };
+
+  render() {
+      return (
+        <Button 
+          className={classes.Button}
+          onClick={this.onClickHandler}>
+          Aktion ausführen
+        </Button>
+      );
+  }
+}
--- home-automation-ui/src/utils/deviceControls.types.js
+++ home-automation-ui/src/utils/deviceControls.types.js
@@ -1,3 +1,4 @@
 export const TEMPERATURE = "temperature";
 export const SCALE = "scale";
 export const MODE = "mode";
+export const BUZZER = "buzzer";
